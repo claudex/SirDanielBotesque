c46e4c873b6b34f04d8eba4e89e8ff21d2417c53
Benzine <fryfonbly@gmail.com>
2024-07-18 08:28:21 +0200

    datetime first steps

--- SDBCommands.py
+++ SDBCommands.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 from SirDanBot import SirDan
 import logging
+# testing
+from datetime import datetime
+from datetime import timedelta
 
 class Commands( commands.Cog ):
     def __init__( self, _bot ):
@@ -16,6 +19,12 @@ class Commands( commands.Cog ):
         game_name = " ".join( _game ).title()
         link_end = "+".join( _game )
         await ctx.send( f"Recherche Is There Any Deal: **[{ game_name }](<https://isthereanydeal.com/search/?q={ link_end }>)**")
+
+    @commands.command()
+    async def bereal( self, ctx: commands.Context ):
+        delta = datetime.combine( datetime.today() + timedelta( days=1 ), datetime.min.time() ) - datetime.combine( datetime.today(), datetime.now().time() )
+        print( f"{delta}" )
+        await ctx.send( "bjr" )
 
 
 async def setup( _bot ):
